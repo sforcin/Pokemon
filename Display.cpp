75cd6ad3e9bbe7689b627c0c5146ed28afc9768b
Sricharan-Pamireddy <sricharan.pamireddy@gmail.com>
2024-05-23 18:43:39 -0700

    Added welcome screen and character selection classes, and also integrated them with the main Display class

--- Display.cpp
+++ Display.cpp
@@ -1,14 +1,43 @@
 #include "Display.h"
 
+//
+// MAIN MENU CLASS
+//
+
 void DisplayMainMenu::print() const {
     cout << border << endl;
-    cout << indent << "MAIN MENU:" << endl;
+    cout << indent << "    MAIN MENU:" << endl;
     for (int i = 0; i < options.size(); i++) {
-        cout << indent << options.at(i) << endl;
+        cout << indent << i << ". " << options.at(i) << endl;
     }
     cout << border << endl;
 }
 
 vector<string> DisplayMainMenu::getOptions() const {
+    return options;
+}
+
+//
+// WELCOME SCREEN CLASS
+//
+
+void DisplayWelcomeScreen::print() const {
+    cout << message << endl;
+}
+
+//
+// CHARACTER SELECTION SCREEN CLASS
+//
+
+void DisplayCharacterSelection::print() const {
+    cout << border << endl;
+    cout << message << endl;
+    for (int i = 0; i < options.size(); i++) {
+        cout << indent << i << ". " << options.at(i) << endl;
+    }
+    cout << border << endl;
+}
+
+vector<string> DisplayCharacterSelection::getOptions() const {
     return options;
 }
--- Display.h
+++ Display.h
@@ -7,7 +7,10 @@
 using namespace std;
 
 class Display {
-
+    public:
+        const DisplayMainMenu mainMenu;
+        const DisplayWelcomeScreen welcomeScreen;
+        const DisplayCharacterSelection characterSelection;
 };
 
 class DisplayMainMenu {
@@ -17,6 +20,25 @@ class DisplayMainMenu {
         const string indent = "        "; // the indent to print before each line, excluding the border lines
         void print() const;
         friend class Display;
+    public:
+        vector<string> getOptions() const;
+};
+
+class DisplayWelcomeScreen {
+    private:
+        const string message = "Hello, welcome to the world of Pokemon!\nBefore you begin your adventure, tell us your name: ";
+        void print() const;
+    friend class Display;
+};
+
+class DisplayCharacterSelection {
+    private:
+        const string message = "Choose your starter pokemon!:";
+        const vector<string> options{"Charmander", "Squirtle", "Bulbasaur"};
+        const string border = "----------------------------"; // the border to be printed before and after the menu
+        const string indent = "        "; // the indent to print before each line, excluding the border lines
+        void print() const;
+    friend class Display;
     public:
         vector<string> getOptions() const;
 };
