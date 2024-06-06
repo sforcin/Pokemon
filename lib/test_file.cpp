c68113df2716539716c772f352d1b7a1b1a1c547
sforcin <isabelasforcin@outlook.com>
2024-06-05 17:47:16 -0700

    test files created

--- lib/test_file.cpp
+++ lib/test_file.cpp
@@ -1,17 +1,4 @@
 #include <gtest/gtest.h>
 
 // A simple test case
-TEST(SampleTest, AssertionTrue) {
-    EXPECT_EQ(1, 1);
-}
-
-// Another simple test case
-TEST(SampleTest, AssertionFalse) {
-    EXPECT_NE(1, 0);
-}
-
-// Main function for running all tests
-int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
+//just a template. nnothing here so far
