878b27e0cf6e5c40f0c5fa76064b0cf164674385
sforcin <isabelasforcin@outlook.com>
2024-06-07 14:18:05 -0700

    tests updated, fixed the bug

--- tests/PokemonTest.cpp
+++ tests/PokemonTest.cpp
@@ -4,21 +4,4 @@
 #include <iostream>
 
 
-// TEST(PokemonTest, CharmanderEvolve) {
-//     CharmanderLine charmander;
-//     charmander.setName("Charmander");
-//     charmander.ChooseType("Fire");
-
-//     // Set stats to trigger evolution
-//     charmander.Attack = 60;
-//     charmander.Defense = 50;
-
-//     charmander.evolve();
-
-//     EXPECT_EQ(charmander.Name, "Charmeleon");
-//     EXPECT_EQ(charmander.HP, 58);
-//     EXPECT_EQ(charmander.Attack, 64);
-//     EXPECT_EQ(charmander.Defense, 58);
-//     EXPECT_EQ(charmander.SpeedAttack, 80);
-//     EXPECT_EQ(charmander.SpeedDefense, 65);
-// }
+//we are not testing the pokemon class because its mostly just constructors.
