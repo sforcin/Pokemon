8659fbeb7136d10125830622ae329b6bd76b7c59
sforcin <isabelasforcin@outlook.com>
2024-06-07 03:43:11 -0700

    new tests added, will continue in morning

---
+++ tests/PokemonTest.cpp
@@ -0,0 +1,24 @@
+#include <gtest/gtest.h>
+#include "../includeFiles/Pokemon.h"
+#include <string>
+#include <iostream>
+
+
+// TEST(PokemonTest, CharmanderEvolve) {
+//     CharmanderLine charmander;
+//     charmander.setName("Charmander");
+//     charmander.ChooseType("Fire");
+
+//     // Set stats to trigger evolution
+//     charmander.Attack = 60;
+//     charmander.Defense = 50;
+
+//     charmander.evolve();
+
+//     EXPECT_EQ(charmander.Name, "Charmeleon");
+//     EXPECT_EQ(charmander.HP, 58);
+//     EXPECT_EQ(charmander.Attack, 64);
+//     EXPECT_EQ(charmander.Defense, 58);
+//     EXPECT_EQ(charmander.SpeedAttack, 80);
+//     EXPECT_EQ(charmander.SpeedDefense, 65);
+// }
