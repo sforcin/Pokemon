82e08caed8d142f9ba22f6a185cdc07412b35a55
Uma Akundi <139840775+umizoomiexe@users.noreply.github.com>
2024-06-06 22:52:33 -0700

    Update Display.h

--- includeFiles/Display.h
+++ includeFiles/Display.h
@@ -8,7 +8,7 @@ using namespace std;
 
 class DisplayMainMenu {
     private:
-        const vector<string> options{"Start new game", "Resume game", "View all games", "Pokedex!"}; // list of options
+        const vector<string> options{"Start new game", "Resume game"}; // list of options
         const string border = "---------------------------------"; // the border to be printed before and after the menu
         const string indent = "        "; // the indent to print before each line, excluding the border lines
         friend class Display;
